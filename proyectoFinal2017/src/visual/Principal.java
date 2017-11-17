cfa5bb349ce947fd4670baf0b112679e4a5cb097
chess <chess@DESKTOP-45QK9EU>
2017-11-17 13:48:53 +0000

    Agregacion de todas las ventanas visuales

--- proyectoFinal2017/src/visual/Principal.java
+++ proyectoFinal2017/src/visual/Principal.java
@@ -15,6 +15,8 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Principal extends JFrame {
 
@@ -55,42 +57,115 @@ public class Principal extends JFrame {
 		menuBar.add(mnCliente);
 		
 		JMenuItem mntmRegistrarCliente = new JMenuItem("Registrar Cliente");
+		mntmRegistrarCliente.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RegistrarCliente reg = new RegistrarCliente();
+				reg.setModal(true);
+				reg.setLocationRelativeTo(null);
+				reg.setVisible(true);
+			
+			}
+		});
 		mnCliente.add(mntmRegistrarCliente);
 		
 		JMenuItem mntmListarClientes = new JMenuItem("Listar Clientes");
+		mntmListarClientes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ListarCliente list = new ListarCliente();
+				list.setModal(true);
+				list.setLocationRelativeTo(null);
+				list.setVisible(true);
+			}
+		});
 		mnCliente.add(mntmListarClientes);
 		
 		JMenu mnNewMenu = new JMenu("Productos");
 		menuBar.add(mnNewMenu);
 		
 		JMenuItem mntmRegistrarProducto = new JMenuItem("Registrar producto");
+		mntmRegistrarProducto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RegistrarProducto reg = new RegistrarProducto();
+				reg.setModal(true);
+				reg.setLocationRelativeTo(null);
+				reg.setVisible(true);
+			}
+		});
 		mnNewMenu.add(mntmRegistrarProducto);
 		
 		JMenuItem mntmListarProducto = new JMenuItem("Listar Producto");
+		mntmListarProducto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ListarProducto list = new ListarProducto();
+				list.setModal(true);
+				list.setLocationRelativeTo(null);
+				list.setVisible(true);
+			}
+		});
 		mnNewMenu.add(mntmListarProducto);
 		
 		JMenu mnVenta = new JMenu("Ventas");
 		menuBar.add(mnVenta);
 		
 		JMenuItem mntmCrearFactura = new JMenuItem("Crear factura");
+		mntmCrearFactura.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CrearFactura reg = new CrearFactura();
+				reg.setModal(true);
+				reg.setLocationRelativeTo(null);
+				reg.setVisible(true);
+			}
+		});
 		mnVenta.add(mntmCrearFactura);
 		
 		JMenuItem mntmListarFactura = new JMenuItem("Listar Factura");
+		mntmListarFactura.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ListarFactura list = new ListarFactura();
+				list.setModal(true);
+				list.setLocationRelativeTo(null);
+				list.setVisible(true);
+			}
+		});
 		mnVenta.add(mntmListarFactura);
 		
 		JMenu mnCombos = new JMenu("Combos");
 		menuBar.add(mnCombos);
 		
 		JMenuItem mntmVerCombos = new JMenuItem("Ver Combos");
+		mntmVerCombos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Combo comb = new Combo();
+				comb.setModal(true);
+				comb.setLocationRelativeTo(null);
+				comb.setVisible(true);
+			}
+		});
 		mnCombos.add(mntmVerCombos);
 		
 		JMenu mnInventario = new JMenu("Inventario");
 		menuBar.add(mnInventario);
 		
 		JMenuItem mntmInventario = new JMenuItem("Inventario");
+		mntmInventario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Inventario invent = new Inventario();
+				invent.setModal(true);
+				invent.setLocationRelativeTo(null);
+				invent.setVisible(true);
+			}
+		});
 		mnInventario.add(mntmInventario);
 		
 		JMenuItem mntmOrdenesDeCompras = new JMenuItem("Ordenes de Compras");
+		mntmOrdenesDeCompras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				OrdenesCompra ordenCompra = new OrdenesCompra();
+				ordenCompra.setModal(true);
+				ordenCompra.setLocationRelativeTo(null);
+				ordenCompra.setVisible(true);
+			}
+		});
 		mnInventario.add(mntmOrdenesDeCompras);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
---
+++ proyectoFinal2017/src/visual/RegistrarCliente.java
@@ -0,0 +1,70 @@
+package visual;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.border.TitledBorder;
+
+public class RegistrarCliente extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			RegistrarCliente dialog = new RegistrarCliente();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public RegistrarCliente() {
+		setTitle("Registrar cliente");
+		setResizable(false);
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			panel.setBorder(new TitledBorder(null, "Informaci\u00F3n del cliente", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+			contentPanel.add(panel, BorderLayout.CENTER);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("Guardar");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancelar");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						dispose();
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
