12e43aa3249423a330376e7fafff4023d71e6301
leomar <osnel06@gmail.com>
2017-11-30 13:48:14 -0400

    Detalles visuales

--- proyectoFinal2017/src/visual/CrearFactura.java
+++ proyectoFinal2017/src/visual/CrearFactura.java
@@ -26,18 +26,18 @@ public class CrearFactura extends JDialog {
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
-	private JTextField textField_5;
-	private JTextField textField_4;
-	private JTextField textField_8;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_9;
+	private JTextField txtDireccion;
+	private JTextField txtCedula;
+	private JTextField txtNombre;
+	private JTextField txtTelefono;
+	private JTextField txtApellido;
+	private JTextField txtCorreo;
 	private JFormattedTextField formattedTextField;
-	private JTextField textField_10;
-	private JTextField textField_11;
-	private JTextField textField_12;
-	private JTextField textField_13;
-	private JTextField textField_14;
+	private JTextField txtCodigo;
+	private JTextField txtProducto;
+	private JTextField txtPrecio;
+	private JTextField txtModelo;
+	private JTextField txtMarca;
 	private JTextField textField_15;
 	private JTextField textField_16;
 	private JTextField textField_17;
@@ -154,10 +154,10 @@ public class CrearFactura extends JDialog {
 		lblDireccion.setBounds(10, 100, 65, 21);
 		panel.add(lblDireccion);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(76, 100, 470, 20);
-		panel.add(textField_5);
+		txtDireccion = new JTextField();
+		txtDireccion.setColumns(10);
+		txtDireccion.setBounds(76, 100, 470, 20);
+		panel.add(txtDireccion);
 		
 		JLabel lblApellido = new JLabel("Apellido:");
 		lblApellido.setBounds(300, 50, 65, 21);
@@ -171,34 +171,34 @@ public class CrearFactura extends JDialog {
 		label_5.setBounds(300, 75, 46, 21);
 		panel.add(label_5);
 		
-		JButton btnBuscar = new JButton("Buscar");
-		btnBuscar.setBounds(300, 24, 89, 23);
-		panel.add(btnBuscar);
+		JButton btnBuscarCedula = new JButton("Buscar");
+		btnBuscarCedula.setBounds(300, 24, 89, 23);
+		panel.add(btnBuscarCedula);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(76, 25, 199, 20);
-		panel.add(textField_4);
+		txtCedula = new JTextField();
+		txtCedula.setColumns(10);
+		txtCedula.setBounds(76, 25, 199, 20);
+		panel.add(txtCedula);
 		
-		textField_8 = new JTextField();
-		textField_8.setColumns(10);
-		textField_8.setBounds(76, 50, 199, 20);
-		panel.add(textField_8);
+		txtNombre = new JTextField();
+		txtNombre.setColumns(10);
+		txtNombre.setBounds(76, 50, 199, 20);
+		panel.add(txtNombre);
 		
-		textField_6 = new JTextField();
-		textField_6.setColumns(10);
-		textField_6.setBounds(76, 75, 199, 20);
-		panel.add(textField_6);
+		txtTelefono = new JTextField();
+		txtTelefono.setColumns(10);
+		txtTelefono.setBounds(76, 75, 199, 20);
+		panel.add(txtTelefono);
 		
-		textField_7 = new JTextField();
-		textField_7.setColumns(10);
-		textField_7.setBounds(356, 50, 190, 20);
-		panel.add(textField_7);
+		txtApellido = new JTextField();
+		txtApellido.setColumns(10);
+		txtApellido.setBounds(356, 50, 190, 20);
+		panel.add(txtApellido);
 		
-		textField_9 = new JTextField();
-		textField_9.setColumns(10);
-		textField_9.setBounds(356, 75, 190, 20);
-		panel.add(textField_9);
+		txtCorreo = new JTextField();
+		txtCorreo.setColumns(10);
+		txtCorreo.setBounds(356, 75, 190, 20);
+		panel.add(txtCorreo);
 		{
 			JPanel panel_1 = new JPanel();
 			panel_1.setBorder(new TitledBorder(null, "Infomaci\u00F3n del producto", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -215,7 +215,7 @@ public class CrearFactura extends JDialog {
 			panel_1.add(lblComponente);
 			
 			JLabel lblModelo = new JLabel("Modelo:");
-			lblModelo.setBounds(323, 50, 69, 14);
+			lblModelo.setBounds(300, 50, 69, 14);
 			panel_1.add(lblModelo);
 			
 			JLabel lblMarca = new JLabel("Marca:");
@@ -223,46 +223,46 @@ public class CrearFactura extends JDialog {
 			panel_1.add(lblMarca);
 			
 			JLabel lblPrecio = new JLabel("Cantidad:");
-			lblPrecio.setBounds(323, 75, 69, 14);
+			lblPrecio.setBounds(300, 75, 69, 14);
 			panel_1.add(lblPrecio);
 			
-			textField_10 = new JTextField();
-			textField_10.setBounds(72, 22, 154, 20);
-			panel_1.add(textField_10);
-			textField_10.setColumns(10);
+			txtCodigo = new JTextField();
+			txtCodigo.setBounds(72, 22, 199, 20);
+			panel_1.add(txtCodigo);
+			txtCodigo.setColumns(10);
 			
-			textField_11 = new JTextField();
-			textField_11.setColumns(10);
-			textField_11.setBounds(72, 47, 154, 20);
-			panel_1.add(textField_11);
+			txtProducto = new JTextField();
+			txtProducto.setColumns(10);
+			txtProducto.setBounds(72, 47, 199, 20);
+			panel_1.add(txtProducto);
 			
-			textField_12 = new JTextField();
-			textField_12.setColumns(10);
-			textField_12.setBounds(72, 97, 154, 20);
-			panel_1.add(textField_12);
+			txtPrecio = new JTextField();
+			txtPrecio.setColumns(10);
+			txtPrecio.setBounds(72, 97, 199, 20);
+			panel_1.add(txtPrecio);
 			
-			textField_13 = new JTextField();
-			textField_13.setColumns(10);
-			textField_13.setBounds(389, 47, 154, 20);
-			panel_1.add(textField_13);
+			txtModelo = new JTextField();
+			txtModelo.setColumns(10);
+			txtModelo.setBounds(356, 47, 190, 20);
+			panel_1.add(txtModelo);
 			
-			textField_14 = new JTextField();
-			textField_14.setColumns(10);
-			textField_14.setBounds(72, 72, 154, 20);
-			panel_1.add(textField_14);
+			txtMarca = new JTextField();
+			txtMarca.setColumns(10);
+			txtMarca.setBounds(72, 72, 199, 20);
+			panel_1.add(txtMarca);
 			
-			JButton btnBuscar_1 = new JButton("Buscar");
-			btnBuscar_1.setBounds(303, 21, 89, 23);
-			panel_1.add(btnBuscar_1);
+			JButton btnBuscarCodigo = new JButton("Buscar");
+			btnBuscarCodigo.setBounds(303, 21, 89, 23);
+			panel_1.add(btnBuscarCodigo);
 			
 			JLabel lblCantidad = new JLabel("Precio:");
 			lblCantidad.setBounds(10, 100, 69, 14);
 			panel_1.add(lblCantidad);
 			
-			JSpinner spinner = new JSpinner();
-			spinner.setModel(new SpinnerNumberModel(new Integer(0), new Integer(0), null, new Integer(1)));
-			spinner.setBounds(389, 72, 154, 20);
-			panel_1.add(spinner);
+			JSpinner spnCantidad = new JSpinner();
+			spnCantidad.setModel(new SpinnerNumberModel(new Integer(0), new Integer(0), null, new Integer(1)));
+			spnCantidad.setBounds(356, 72, 190, 20);
+			panel_1.add(spnCantidad);
 			
 			JButton btnAgregarAlCarrito = new JButton("Agregar al carrito");
 			btnAgregarAlCarrito.setBounds(303, 100, 154, 23);
@@ -313,20 +313,24 @@ public class CrearFactura extends JDialog {
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("Vender");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
+				JButton btnVender = new JButton("Vender");
+				btnVender.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+					}
+				});
+				btnVender.setActionCommand("OK");
+				buttonPane.add(btnVender);
+				getRootPane().setDefaultButton(btnVender);
 			}
 			{
-				JButton cancelButton = new JButton("Cancelar");
-				cancelButton.addActionListener(new ActionListener() {
+				JButton btnCancelar = new JButton("Cancelar");
+				btnCancelar.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						dispose();
 					}
 				});
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
+				btnCancelar.setActionCommand("Cancel");
+				buttonPane.add(btnCancelar);
 			}
 		}
 	}
